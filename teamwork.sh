4c616c6228bb994eab3f21479bb10378c8233730
xD'Criz <32378563+criz16@users.noreply.github.com>
2023-05-24 22:07:54 +0800

    Update teamwork.sh

--- teamwork.sh
+++ teamwork.sh
@@ -11,7 +11,7 @@ sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
 # install wget and curl
 apt-get update; apt-get -y install wget curl;
-apt-get install net-tools python -y
+apt-get install net-tools python screen -y
 
 # set time GMT +7
 ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
